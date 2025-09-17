bed95479710bad302489682c9dc7cf2f3bab3f07
vsandhu-developer <developervsandhu@gmail.com>
2025-09-17 16:08:33 -0400

    updated code

--- index.ts
+++ index.ts
@@ -1,52 +1,54 @@
+import type { AIMessage } from "@langchain/core/messages";
 import { ChatGroq } from "@langchain/groq";
+import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
 import { createEventTool, getEventTool } from "./src/tools";
-import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
-import { ToolNode } from "@langchain/langgraph/prebuilt"
-import { AIMessage } from "@langchain/core/messages";
 
 const tools: any = [createEventTool, getEventTool];
-const toolNode = new ToolNode(tools)
+const toolNode = new ToolNode(tools);
 
 const groq = new ChatGroq({
-	apiKey: process.env.GROQ_API_KEY,
-	model: "openai/gpt-oss-20b",
-	temperature: 0,
+  apiKey: process.env.GROQ_API_KEY,
+  model: "openai/gpt-oss-20b",
+  temperature: 0,
 }).bindTools(tools);
 
-
 async function callModel(state: typeof MessagesAnnotation.State) {
-	const response = await groq.invoke(state.messages);
-	return { messages: [response] }
+  const response = await groq.invoke(state.messages);
+  return { messages: [response] };
 }
 
 function shouldContinue(state: typeof MessagesAnnotation.State) {
-	const lastMessage = state.messages[state.messages.length - 1] as AiMessage;
+  const lastMessage = state.messages[state.messages.length - 1] as AIMessage;
 
-	  if (lastMessage.tool_calls?.length) {
-	  	return "tools";
-	  }
+  if (lastMessage.tool_calls?.length) {
+    return "tools";
+  }
 
-	  return "__end__";
+  return "__end__";
 }
 
-
 const workflow = new StateGraph(MessagesAnnotation)
-	.addNode("assistant", callModel)
-	.addNode("tools", toolNode)
-	.addEdge("__start__", "assistant")
-	.addEdge("tools", "assistant")
-	.addConditionalEdges("assistant", shouldContinue);
-
+  .addNode("assistant", callModel)
+  .addNode("tools", toolNode)
+  .addEdge("__start__", "assistant")
+  .addEdge("tools", "assistant")
+  .addConditionalEdges("assistant", shouldContinue);
 
 const app = workflow.compile();
 
-
 async function main() {
-	const result = await app.invoke({
-		messages: [{ role: "user", content: "can you check If I do have any meetings in my calendar tomorrow ?" }]
-	});
+  const result = await app.invoke({
+    messages: [
+      {
+        role: "user",
+        content:
+          "can you check If I do have any meetings in my calendar tomorrow ?",
+      },
+    ],
+  });
 
-	console.log("AI: ", result.messages[result.messages.length - 1].content);
+  console.log("AI: ", result.messages[result.messages.length - 1]?.content);
 }
 
 main();
--- server.ts
+++ server.ts
@@ -1,40 +1,33 @@
-import express, { Request, Response } from "express";
-import {google} from "googleapis";
-import session from "cookie-session";
+import express, { type Request, type Response } from "express";
+import { google } from "googleapis";
 
 const app = express();
 
-export const oauth2Client = new google.auth.OAuth2(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_REDIRECT_URL);
-
-app.use(session({
-	name: session,
-	keys: ["super-secure-key"]
-}))
+export const oauth2Client = new google.auth.OAuth2(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_REDIRECT_URL
+);
 
 app.get("/auth", (req: Request, res: Response) => {
-	const scopes = [
-	  'https://www.googleapis.com/auth/calendar'
-	];
+  const scopes = ["https://www.googleapis.com/auth/calendar"];
 
-	const url = oauth2Client.generateAuthUrl({
-	  access_type: 'offline',
-	  scope: scopes,
-	  prompt: "consent"
-	});
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+    prompt: "consent",
+  });
 
-
-	res.redirect(url);
+  res.redirect(url);
 });
-
 
 app.get("/oauth2callback", async (req: Request, res: Response) => {
-	const code = req.query.code as string;
-	
-	const { tokens } = await oauth2Client.getToken(code);
-	oauth2Client.setCredentials(tokens);
-	
-	res.send("You can now close this tab");
-});
+  const code = req.query.code as string;
 
+  const { tokens } = await oauth2Client.getToken(code);
+  oauth2Client.setCredentials(tokens);
+
+  res.send("You can now close this tab");
+});
 
 app.listen(3000, () => console.log("Server is running on 3000"));
