27b2133ccb1eebbf517166ee67cd9b4eab2cfc10
vsandhu-developer <developervsandhu@gmail.com>
2025-09-17 16:04:57 -0400

    Get event tool added

--- index.ts
+++ index.ts
@@ -1,12 +1,52 @@
 import { ChatGroq } from "@langchain/groq";
-import { createEventTool, getEventTool } from "./src/tools"
-
+import { createEventTool, getEventTool } from "./src/tools";
+import { StateGraph, MessagesAnnotation } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt"
+import { AIMessage } from "@langchain/core/messages";
 
 const tools: any = [createEventTool, getEventTool];
-
+const toolNode = new ToolNode(tools)
 
 const groq = new ChatGroq({
 	apiKey: process.env.GROQ_API_KEY,
 	model: "openai/gpt-oss-20b",
 	temperature: 0,
 }).bindTools(tools);
+
+
+async function callModel(state: typeof MessagesAnnotation.State) {
+	const response = await groq.invoke(state.messages);
+	return { messages: [response] }
+}
+
+function shouldContinue(state: typeof MessagesAnnotation.State) {
+	const lastMessage = state.messages[state.messages.length - 1] as AiMessage;
+
+	  if (lastMessage.tool_calls?.length) {
+	  	return "tools";
+	  }
+
+	  return "__end__";
+}
+
+
+const workflow = new StateGraph(MessagesAnnotation)
+	.addNode("assistant", callModel)
+	.addNode("tools", toolNode)
+	.addEdge("__start__", "assistant")
+	.addEdge("tools", "assistant")
+	.addConditionalEdges("assistant", shouldContinue);
+
+
+const app = workflow.compile();
+
+
+async function main() {
+	const result = await app.invoke({
+		messages: [{ role: "user", content: "can you check If I do have any meetings in my calendar tomorrow ?" }]
+	});
+
+	console.log("AI: ", result.messages[result.messages.length - 1].content);
+}
+
+main();
--- src/tools.ts
+++ src/tools.ts
@@ -1,5 +1,63 @@
 import { tool } from "@langchain/core/tools";
 import z from "zod";
+import { google } from "googleapis";
+import { oauth2Client } from "../server";
+import tokens from "../tokens.json" assert { type: "json" };
+
+oauth2Client.setCredentials(tokens);	
+
+const calendar = google.calendar({ version: "v3", auth: oauth2Client });
+
+interface Params {
+	q: string;
+	timeMin: string;
+	timeMax: string;
+}
+
+export const getEventTool = tool(
+	async (params) => {
+
+		const { q, timeMin, timeMax } = params as Params;
+
+		try {
+			const response = await calendar.events.list({
+				calendarId: "vinaysandhuindia@gmail.com",
+				q: q,
+				timeMin: timeMin,
+				timeMax: timeMax
+			});
+
+			const result = response.data.items.map((event) => {
+				return {
+					id: event.id,
+					summary: event.summary,
+					status: event.status,
+					creator: event.creator,
+					organizer: event.organizer,
+					startTime: event.start,
+					endTime: event.end,
+					meetingUrl: event.hangoutLink,
+					eventType: event.eventType
+				}
+			})
+
+			return JSON.stringify(result);
+		} catch(error) {
+			console.log("ERRERERE", error);
+		}
+
+		return "Failed to connect to calendar";
+	},
+	 {
+	 	name: "get-events",
+	 	description: "this tool can be used to check the meetings in the calendar",
+	 	schema: z.object({
+	 		q: z.string().describe("The query parameter can be used to events from google calendar. You can pass parameter in this function like summary, description, location, attendee's displayName, attendee's email, organizer's displayName, organizer's email"),
+	 		timeMin: z.string().describe("The from datetime in UTC format for the event"),
+	 		timeMax: z.string().describe("The to datetime in UTC format for the event")
+	 	})
+	 }
+);
 
 export const createEventTool = tool(
 	async () => {
@@ -13,19 +71,3 @@ export const createEventTool = tool(
 );
 
 
-export const getEventTool = tool(
-	async () => {
-		return JSON.stringify({
-			name: "Website Review",
-			description: "The website is working just need to do some minor changes",
-			time: "3AM",
-			date: "17 Sept",
-			platform: "Google Meet"
-		})
-	},
-	 {
-	 	name: "get-events",
-	 	description: "this tool can be used to check the meetings in the calendar",
-	 	schema: z.object({})
-	 }
-);
